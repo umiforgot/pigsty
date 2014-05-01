2a191cccaa5b1c64ce4cd093f4ed55c4782bbb01
umiforgot <marcmahoney@hotmail.com>
2014-05-01 09:31:14 -0400

    Adding chore controller

---
+++ pigsty/src/main/java/com/marcmahoney/pigsty/controller/ChoreController.java
@@ -0,0 +1,14 @@
+/**
+ * pigsty
+ * ChoreController.java
+ *
+ * 
+ *
+ * @author Marc Mahoney
+ * @version Apr 30, 2014
+ */
+package com.marcmahoney.pigsty.controller;
+
+public class ChoreController {
+
+}
